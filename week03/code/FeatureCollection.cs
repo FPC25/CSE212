a6a9022090f519204ae812fb548aeb1afbcdd99c
Felipe Proença Corral <42944221+FPC25@users.noreply.github.com>
2026-01-22 21:03:41 -0300

    adiciona classes Feature, Properties e Geometry à coleção de recursos

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,29 @@
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public string Type { get; set; }
+    public Feature[] Features { get; set; }
+
+}
+
+public class Feature
+{
+    public string Id { get; set; }
+    public Properties Properties { get; set; }
+    public Geometry Geometry { get; set; }
+}
+
+public class Properties
+{
+    public double? Mag { get; set; }
+    public string Place { get; set; }
+    public long? Time { get; set; }
+    public long? Updated { get; set; }
+    public int? Tz { get; set; } 
+    public string MagType { get; set; }
+}
+
+public class Geometry
+{
+    public string Type { get; set; }
+    public decimal?[] Coordinates { get; set; }
 }
